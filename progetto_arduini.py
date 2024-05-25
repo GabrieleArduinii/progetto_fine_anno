85eda017989deeb706a03e19d77e082510326738
GabrieleArduinii <147494594+GabrieleArduinii@users.noreply.github.com>
2024-05-25 13:50:47 +0200

    Add files via upload

---
+++ progetto_arduini.py
@@ -0,0 +1,59 @@
+import random
+import time  
+
+def crea_mazzo():
+    semi = ["Denari", "Coppe", "Bastoni", "Spade"]
+    valori = ["Asso", "2", "3", "4", "5", "6", "7", "Fante", "Cavallo", "Re"]
+    mazzo = [(valore, seme) for seme in semi for valore in valori]
+    random.shuffle(mazzo)  
+    return mazzo
+
+def distribuisci_carte(mazzo):
+    return mazzo[:26], mazzo[26:]  
+
+def stampa_stato_gioco(giocatore1, giocatore2):
+    print("\n--- Stato del gioco ---")
+    print("Giocatore 1:", len(giocatore1), "carte")
+    print("Giocatore 2:", len(giocatore2), "carte")
+
+def gioca_turno(mano_giocatore, mazzo_giocatore, mazzo_avversario):
+
+    carta_giocata = mano_giocatore.pop(0)  
+    mazzo_giocatore.append(carta_giocata)  
+
+    if mazzo_avversario and mazzo_avversario[-1][0] == carta_giocata[0]:  
+        mazzo_giocatore += mazzo_avversario  
+        mazzo_avversario = []
+        print("Rubamazzo")
+        time.sleep(1)  
+
+def rubamazzo(giocatore1, giocatore2):
+   
+    turno = 1  # Contatore dei turni
+    while giocatore1 and giocatore2:
+        print(f"\n--- Turno {turno} ---")
+        stampa_stato_gioco(giocatore1, giocatore2) 
+
+        input("Premi Invio per giocare il turno del Giocatore 1")
+        gioca_turno(giocatore1, giocatore1, giocatore2)
+
+        input("Premi Invio per giocare il turno del Giocatore 2")
+        gioca_turno(giocatore2, giocatore2, giocatore1)
+
+        turno += 1
+
+def determina_vincitore(giocatore1, giocatore2):
+
+    if len(giocatore1) > len(giocatore2):
+        return "Giocatore 1"
+    elif len(giocatore1) < len(giocatore2):
+        return "Giocatore 2"
+    else:
+        return "Pareggio"
+
+mazzo = crea_mazzo()
+giocatore1, giocatore2 = distribuisci_carte(mazzo)
+
+rubamazzo(giocatore1, giocatore2)
+vincitore = determina_vincitore(giocatore1, giocatore2)
+print(f"Il vincitore è: {vincitore}")
